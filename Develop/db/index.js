f8b3cda8b9c5d2f38fedfcbf810b20cd63e508db
B Water <brennw47@gmail.com>
2024-05-21 18:31:04 -0600

    Updated index.js to pseudo code next steps for the prompts, and how to tie in the functions that will return the field and records for the tables, and added in a switch statement that takes in the userdata from the prompted questions, added a function to find all employees/Updated db/index.js to pseudo code in what to do with the interpolation for adding stuff to the tables during the prompts, and then also add a comment to make sure the database is exported to the index.js so the functions for the prompts can be used.
    Updated index.js to pseudo code next steps for the prompts, and how to tie in the functions that will return the field and records for the tables, and added in a switch statement that takes in the userdata from the prompted questions, added a function to find all employees. Updated db/index.js to pseudo code in what to do with the interpolation for adding stuff to the tables during the prompts, and then also add a comment to make sure the database is exported to the index.js so the functions for the prompts can be used.

--- Develop/db/index.js
+++ Develop/db/index.js
@@ -19,8 +19,12 @@ findAllEmployees() {
         "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department FROM employees JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id"
     );
 }
+// use interpolation to add in the adding of things to prompt questions 
+// in the function for updating employee this function needs to update data base i need all roles make array to pass to the prompts 
+// bunch of handler functions to make for switch case now i can worry about 
 // each prompt question is a method building out each method for essentially 
-// what is the sql query for each of my individual things i want to ask the system
-// when they can recieve the data
-// catching values from params when db is called
-// then return this query for sql good old all caps deposit values and which values are being referenced.
+// what is the sql query for each of my individual things i want to ask the system 
+// when they can recieve the data 
+// catching values from params when db is called 
+// then return this query for sql good old all caps deposit values and which values are being referenced. 
+// export the database for the functions so i can add to prompt. 
--- Develop/index.js
+++ Develop/index.js
@@ -18,9 +18,38 @@ function loadMainPrompts() {
             type: 'list',
             name: 'choice',
             message: 'What would you like to do?',
-            choices: [
+            choices: 
+            [{name: 'findAllEmployees',
+                value: 'findAllEmployees'
+            }
 
             ]
         }
     ])
+}
+//seperate prompts
+// get switch to run stuff, and add dynamics 
+// .then comes after the prompt prompt returns a promise so .then will execute after promise is resolved so after the question has been answered.
+// switch case for after this question then do what. sort of like you click this prompt for example then switch to this case if this function runs main 
+// menu swithc case says run corresponding function then do all the things in there throw extra prompts snap boom back to main menu and it will start it over.
+// how would I connect this to a prompt.
+// choice will be value key will be the name of the object.
+// can actually throw if you do a look up have a function
+.then((userData) => {
+
+    switch (userData.start) {
+        //case has to be what the value is that the user chose
+        case 'findAllEmployees';
+        findAllEmployees();
+        break;
+    }
+}
+// ask question based on function get user data from what was wrote in prompt then return to main menu
+function findAllEmployees(){
+    console.log('Found all employees')
+    //having data from find all employees and show data to user with ascii art logo then call initial function again to ask all quesiton for what you want to do again that will be in the .then
+    db.findAllEmployees().then(({ rows }) => {
+        let departments = rows;
+        console.table(departments)
+    }).then() => loadMainPrompts()}
 }
